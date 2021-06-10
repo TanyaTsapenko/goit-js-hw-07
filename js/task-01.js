e18fe272d7fb4a40a1a37a8fdd22848c28fc73d2
Tanya Tsapenko <Tanyaslavinat@gmail.com>
2021-06-10 15:16:39 +0300

    ссылки

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,21 @@
-const totalCategoriesAndEl = () => {
-  const titleCategory = document.querySelectorAll(".item");
-  console.log(`В списке ${titleCategory.length} категории.`);
-
-  titleCategory.forEach((element) => {
+//Напиши скрипт, который выполнит следующие операции.
+//Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+//Получится 'В списке 3 категории.'.
+const totalcategoriesEl = () => {
+  const categoriesEl = document.querySelectorAll("li.item");
+  console.log(`В списке ${categoriesEl.length} категории`);
+  //Для каждого элемента li.item в списке ul#categories,
+  //найдет и выведет в консоль текст заголовка элемента(тега h2)
+  //и количество элементов в категории(всех вложенных в него элементов li).
+  //Например для первой категории получится:
+  //Категория: Животные
+  //Количество элементов: 4
+  categoriesEl.forEach((element) => {
+    const textCategoryEl = document.querySelector("h2");
     console.log(`Категория: ${element.querySelector("h2").textContent}`);
     console.log(
-      `Количество элементов: ${element.querySelectorAll("li").length}`
+      `Количество элементов ${element.querySelectorAll("li").length}`
     );
   });
 };
-totalCategoriesAndEl();
+totalcategoriesEl();
